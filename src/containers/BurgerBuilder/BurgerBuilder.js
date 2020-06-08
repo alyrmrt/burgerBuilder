2ba7cde4d345be3649eb6d6c35110781ae265507
alyrtmrt <alayurtmm@gmail.com>
2020-06-08 13:26:28 -0400

    added logo components added Navigation and Toolbar components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,7 +75,13 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true})
       }
 
+      purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+      }
 
+      purchaseContinueHandler = () => {
+        alert('You Continue')
+      }
 
 
   render () {
@@ -87,8 +93,12 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-         <Modal show={this.state.purchasing}>
-           <OrderSummary ingredients={this.state.ingredients} />
+         <Modal show={this.state.purchasing} modelClosed={this.purchaseCancelHandler}>
+           <OrderSummary
+           ingredients={this.state.ingredients}
+           price={this.state.totalPrice}
+           purchaseCancelled={this.purchaseCancelHandler}
+           purchaseContinued={this.purchaseContinueHandler}/>
          </Modal>
          <Burger ingredients={this.state.ingredients} />
          <BuildControls
